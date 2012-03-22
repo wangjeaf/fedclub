36804950cf80eeec887fa6fc2dec8bb00f440803
wangjeaf <wangjeaf@gmail.com>
2012-03-22 14:16:09 +0800

    format

--- salon/models.py
+++ salon/models.py
@@ -3,25 +3,41 @@ from django.db import models
 
 # Create your models here.
 class Salon(models.Model):
-	salon_id = models.AutoField(primary_key = True) 	#主键
-	name = models.CharField(max_length = 100) 			#活动名称
-	start_time = models.DateTimeField()					#开始时间
-	end_time = models.DateTimeField()   				#结束时间
-	creator = models.CharField(max_length = 40)         #活动创建人
-	description = models.CharField(max_length = 200)    #活动描述
-	address = models.CharField(max_length = 200)        #地址
+	#主键
+	salon_id = models.AutoField(primary_key = True)
+	#活动名称
+	name = models.CharField(max_length = 100)
+	#开始时间
+	start_time = models.DateTimeField()
+	#结束时间
+	end_time = models.DateTimeField()
+	#活动创建人
+	creator = models.CharField(max_length = 40)
+	#活动描述
+	description = models.CharField(max_length = 200)
+	#活动地址
+	address = models.CharField(max_length = 200)
 
 class User(models.Model):
-	user_id = models.AutoField(primary_key = True)		#主键
-	salon = models.ForeignKey(Salon)					#外键，关联salon
+	#主键
+	user_id = models.AutoField(primary_key = True)
+	#外键，关联salon
+	salon = models.ForeignKey(Salon)
 
-	name = models.CharField(max_length = 40) 			#报名人姓名
-	mobile = models.PositiveIntegerField(null=True) 	#手机号
-	email = models.EmailField()							#邮件
-	company = models.CharField(null=True, max_length = 100) #公司名
-	introduction = models.CharField(null=True, max_length = 200) #自我介绍
+	#报名人相关信息
 
-	#状态
+	#姓名
+	name = models.CharField(max_length = 40) 
+	#手机号
+	mobile = models.PositiveIntegerField(null=True)
+	#邮件
+	email = models.EmailField()	
+	#公司名
+	company = models.CharField(null=True, max_length = 100) 
+	#自我介绍
+	introduction = models.CharField(null=True, max_length = 200) 
+
+	#处理状态
 	# 1、未处理 00
 	# 2、已同意未发邮件 10
 	# 3、已同意已发邮件 11
