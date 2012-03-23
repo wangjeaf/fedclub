f4e8bb51a1a3c800d67323090ae537debbfaad13
wangjeaf <wangjeaf@gmail.com>
2012-03-23 14:25:31 +0800

    add views

--- salon/views.py
+++ salon/views.py
@@ -1,10 +1,13 @@
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import Context, loader
 from salon.models import Salon, User
 from salon.utils import get_bar_code, send_mail
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+import datetime
 
 # ^$
 def home(request):
@@ -14,25 +17,64 @@ def home(request):
 # ^salon/$
 def salon_list(request):
 	return HttpResponse('salon_list')
+
 # ^salon/add/
 def salon_add(request):
-	t = loader.get_template('salon/add.html')
-	c = Context({})
-	return HttpResponse(t.render(c))
+	try:
+		edit_type = request.POST['edit_type']
+	except(KeyError):
+		return render_to_response('salon/add.html', {}, context_instance=RequestContext(request))
+	else:
+		salon = Salon()
+		salon.name = request.POST['name']
+		salon.start_time = request.POST['start_time']
+		salon.end_time = request.POST['end_time']
+		salon.creator = request.POST['creator']
+		salon.description = request.POST['description']
+		salon.address = request.POST['address']
+		salon.save()
+		return HttpResponseRedirect(reverse('home'))
+
 # ^salon/(?P<salon_id>[\w\d]+)/$
 def salon_get(request, salon_id):
-	return HttpResponse('salon_get')
+	salon = Salon.objects.get(salon_id = salon_id)
+	users = User.objects.filter(salon = salon_id)
+	return render_to_response('salon/view.html', {'salon':salon, 'users':users})
+
 # ^salon/(?P<salon_id>[\w\d]+)/update/$
 def salon_update(request, salon_id):
 	return HttpResponse('salon_update')
 # ^salon/(?P<salon_id>[\w\d]+)/delete/$
 def salon_delete(request, salon_id):
-	return HttpResponse('salon_delete')
+	salon = Salon.objects.get(salon_id = salon_id)
+	salon_name = salon.name
+	salon.delete()
+	return render_to_response('salon/delete_success.html', {'salon_name':salon_name})
 
 # many users
 # ^salon/(?P<salon_id>[\w\d]+)/users/$
 def users_list(request, salon_id):
 	return HttpResponse('users_list')
+
+# ^salon/(?P<salon_id>[\w\d]+)/users/add/$
+def users_add(request, salon_id):
+	try:
+		edit_type = request.POST['edit_type']
+	except(KeyError):
+		return render_to_response('user/add.html', {'salon_id':salon_id}, context_instance=RequestContext(request))
+	else:
+		user = User()
+		user.salon = Salon.objects.get(salon_id = salon_id)
+		user.name = request.POST['name']
+		user.company = request.POST['company']
+		user.mobile = request.POST['mobile']
+		user.email = request.POST['email']
+		user.introduction = request.POST['introduction']
+		user.barcode = '1111'
+		user.register_time = datetime.datetime.today()
+		user.status = 0
+		user.save()
+		return HttpResponseRedirect(reverse('home'))
 
 #^salon/(?P<salon_id>[\w\d]+)/users/delete$
 def users_delete(request, salon_id):
@@ -59,40 +101,41 @@ def users_reject_email(request, salon_id):
 	return HttpResponse('users_reject_email')
 
 # single user
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/$
+
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/$
 def user_get(request, salon_id, user_id):
 	return HttpResponse('user_get')
 
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/delete$
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/delete$
 def user_delete(request, salon_id, user_id):
 	return HttpResponse('user_delete')
 
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/update$
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/update$
 def user_update(request, salon_id, user_id):
 	return HttpResponse('user_update')
 
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/accept$
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/accept$
 def user_accept(request, salon_id, user_id):
 	return HttpResponse('user_accept')
 
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/reject$
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/reject$
 def user_reject(request, salon_id, user_id):
 	return HttpResponse('user_reject')
 
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/email$
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/email$
 def user_email(request, salon_id, user_id):
 	return HttpResponse('user_email')
 
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/accept_email$
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/accept_email$
 def user_accept_email(request, salon_id, user_id):
 	return HttpResponse('user_accept_email')
 
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/reject_email$
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/reject_email$
 def user_reject_email(request, salon_id, user_id):
 	return HttpResponse('user_reject_email')
 
 # check in, by barcode
-# ^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/checkin$
+# ^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/checkin$
 def checkin(request, salon_id, user_id):
 	return HttpResponse('checkin')
 
--- urls.py
+++ urls.py
@@ -4,43 +4,42 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('fedclub.salon.views',
     # Examples:
-    url(r'^$',                                                 'fedclub.salon.views.home', name='home'),
+    url(r'^$',                                                 'home', name='home'),
 
 	# salon
-    url(r'^salon/$',                                           'fedclub.salon.views.salon_list'),
-    url(r'^salon/add/$',                                       'fedclub.salon.views.salon_add'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/$',                     'fedclub.salon.views.salon_get'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/update/$',              'fedclub.salon.views.salon_update'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/delete/$',              'fedclub.salon.views.salon_delete'),
+    url(r'^salon/$',                                           'salon_list'),
+    url(r'^salon/add/$',                                       'salon_add'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/$',                     'salon_get'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/update/$',              'salon_update'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/delete/$',              'salon_delete'),
 
 	# many users
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/$',              'fedclub.salon.views.users_list'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/delete$',        'fedclub.salon.views.users_delete'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/accept$',        'fedclub.salon.views.users_accept'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/reject$',        'fedclub.salon.views.users_reject'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/email$',         'fedclub.salon.views.users_email'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/accept_email$',  'fedclub.salon.views.users_accept_email'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/reject_email$',  'fedclub.salon.views.users_reject_email'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/users/$',              'users_list'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/users/add/$',           'users_add'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/users/delete/$',        'users_delete'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/users/accept/$',        'users_accept'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/users/reject/$',        'users_reject'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/users/email/$',         'users_email'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/users/accept_email/$',  'users_accept_email'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/users/reject_email/$',  'users_reject_email'),
 
 	# single user
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/$',           'fedclub.salon.views.user_get'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/delete$',     'fedclub.salon.views.user_delete'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/update$',     'fedclub.salon.views.user_update'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/accept$',     'fedclub.salon.views.user_accept'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/reject$',     'fedclub.salon.views.user_reject'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/email$',      'fedclub.salon.views.user_email'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/accept_email$','fedclub.salon.views.user_accept_email'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/users/(?P<user_id>[\w\d]+)/reject_email$','fedclub.salon.views.user_reject_email'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/$',           'user_get'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/delete/$',     'user_delete'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/update/$',     'user_update'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/accept/$',     'user_accept'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/reject/$',     'user_reject'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/email/$',      'user_email'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/accept_email/$','user_accept_email'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/user/(?P<user_id>[\w\d]+)/reject_email/$','user_reject_email'),
 
 	# check in, by barcode
-    url(r'^salon/(?P<salon_id>[\w\d]+)/checkin_manual$',            'fedclub.salon.views.checkin_manual'),
-    url(r'^salon/(?P<salon_id>[\w\d]+)/checkin$',                   'fedclub.salon.views.checkin'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/checkin_manual$',            'checkin_manual'),
+    url(r'^salon/(?P<salon_id>[\w\d]+)/checkin$',                   'checkin'),
+)
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
